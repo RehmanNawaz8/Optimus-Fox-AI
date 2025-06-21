6ae56e84f3d5b8b263ca611635594ee141b83bfe
Rehman Nawaz <rehmannawaz8@example.com>
2025-06-21 12:39:43 +0500

    first commit

---
+++ 1.py
@@ -0,0 +1,25 @@
+# c= input("Enter your name: ")
+# a= "!Rehman!!!"
+# print(len(c))
+# print(c.upper())
+# print(c.lower())
+# print(c.rstrip("!"))
+# print(c.replace("Rehman","Nawaz"))
+# b= "rehmannawaz"
+# print(b.capitalize())
+# print(b.center(50))
+# print(b.count("n"))
+# print(a.endswith("!!!"))
+
+
+# Makeing an if program
+
+age = int(input('Enter your Age:'))
+if age < 18:
+  print('Cant Vote, Your underage Motherfucker')
+elif age > 18:
+    print('You can vote bitch')
+else:
+    print('Invalid age')
+
+
